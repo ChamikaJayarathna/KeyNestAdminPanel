1be40823ec5f1083de4a71feb553ba5c9fd20f4a
Chamika Jayarathna <failgames.lk@gmail.com>
2025-02-15 23:11:36 +0530

    update main.jsx and sidebar.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import 'admin-lte/dist/css/adminlte.min.css';
+import 'admin-lte/dist/css/adminlte.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
@@ -11,8 +12,9 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'overlayscrollbars/styles/overlayscrollbars.css';
 import 'apexcharts/dist/apexcharts.css'
 import 'apexcharts/dist/apexcharts.js'
+import 'apexcharts/dist/apexcharts.min.js'
 import '@popperjs/core/dist/umd/popper.min.js'
-import 'jquery';
+// import 'jquery';
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
