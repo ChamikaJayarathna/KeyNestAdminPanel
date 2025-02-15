3179ccf096a0f28970a0449121b13195925e5999
Chamika Jayarathna <failgames.lk@gmail.com>
2025-02-15 21:19:02 +0530

    install admin lts

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import 'admin-lte/dist/css/adminlte.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'admin-lte/dist/js/adminlte.min.js';
+import 'admin-lte/dist/js/adminlte.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'overlayscrollbars/styles/overlayscrollbars.css';
+// import 'overlayscrollbars/styles/overlayscrollbars.min.css';
+import 'apexcharts/dist/apexcharts.css'
+import 'apexcharts/dist/apexcharts.js'
+import '@popperjs/core/dist/umd/popper.min.js'
+// import 'overlayscrollbars/browser/overlayscrollbars.browser.es6.min.js'
+// import 'overlayscrollbars/overlayscrollbars.js'
+// import 'apexcharts/dist/apexcharts.min.js'
+import 'jquery';
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
