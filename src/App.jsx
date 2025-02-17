91bd27c2bc38ace032fa237c88de121091b967f9
Chamika Jayarathna <failgames.lk@gmail.com>
2025-02-17 15:40:25 +0530

    creat add property page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './Layout/Layout';
 import Dashboard from './pages/Dashboard';
+import AddProperty from './pages/AddProperty';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Layout/>}>
             <Route path='dashboard' element={<Dashboard/>}/>
+            <Route path='add-property' element={<AddProperty/>}/>
           </Route>
         </Routes>
       </BrowserRouter>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,66 +1,59 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Sidebar = () => {
   
   return (
     <aside className="app-sidebar bg-body-secondary shadow" data-bs-theme="dark">
-    <div className="sidebar-brand">
-      <a className="brand-link">
-        <span className="brand-text font-weight-light">KeyNest Admin</span>
-      </a>
-    </div>
-    <div className="sidebar-wrapper">
-      <nav className="mt-2">
-        <ul className="nav sidebar-menu flex-column" data-lte-toggle="treeview" role="menu" data-accordion="false">
+      <div className="sidebar-brand ">
+        <Link to='/' className="brand-link text-center">
+          <span className="brand-text font-weight-light">KeyNest Admin</span>
+        </Link>
+      </div>
+      <div className="sidebar-wrapper">
+        <nav className="mt-2">
+          <ul className="nav sidebar-menu flex-column" data-lte-toggle="treeview" role="menu" data-accordion="false">
 
-          <li className="nav-item menu-open">
-            <a href="#" className="nav-link active">
-              <i className="nav-icon bi bi-speedometer" />
-              <p>
-                Dashboard
-                <i className="nav-arrow bi bi-chevron-right" />
-              </p>
-            </a>
-            <ul className="nav nav-treeview">
-              <li className="nav-item">
-                <Link to='/dashboard' className="nav-link">
-                  <i className="nav-icon bi bi-circle" />
-                  <p>Dashboard</p>
-                </Link>
-              </li>
-            </ul>
-          </li>
+            <li className="nav-item menu-open">
+              <a href="#" className="nav-link active">
+                <i className="nav-icon bi bi-speedometer" />
+                <p>
+                  Dashboard
+                  <i className="nav-arrow bi bi-chevron-right" />
+                </p>
+              </a>
+              <ul className="nav nav-treeview">
+                <li className="nav-item">
+                  <NavLink to='/dashboard' className="nav-link">
+                    <i className="nav-icon bi bi-circle" />
+                    <p>Dashboard</p>
+                  </NavLink>
+                </li>
+              </ul>
+            </li>
 
-          <li className="nav-item menu-open">
-            <a href="#" className="nav-link">
-              <i className="bi bi-house-add-fill"></i>
-              <p>
-                Home
-                <i className="nav-arrow bi bi-chevron-right" />
-              </p>
-            </a>
-            <ul className="nav nav-treeview">
-              <li className="nav-item">
-                <a href="#" className="nav-link">
-                  <i className="nav-icon bi bi-circle" />
-                  <p>Dashboard</p>
-                </a>
-              </li>
-            </ul>
-          </li>
+            <li className="nav-item menu-open">
+              <a href="#" className="nav-link active">
+                <i className="bi bi-house-add-fill"></i>
+                <p>
+                  Property
+                  <i className="nav-arrow bi bi-chevron-right" />
+                </p>
+              </a>
+              <ul className="nav nav-treeview">
+                <li className="nav-item">
+                  <NavLink to='/add-property' className="nav-link">
+                    <i className="nav-icon bi bi-circle" />
+                    <p>Add Property</p>
+                  </NavLink>
+                </li>
+              </ul>
+            </li>
 
-          <li className="nav-item">
-            <a href="./generate/theme.html" className="nav-link">
-              <i className="nav-icon bi bi-palette" />
-              <p>Theme Generate</p>
-            </a>
-          </li>
-
-        </ul>
-      </nav>
-    </div>
-  </aside>
+          </ul>
+        </nav>
+      </div>
+    </aside>
   );
 }
 
