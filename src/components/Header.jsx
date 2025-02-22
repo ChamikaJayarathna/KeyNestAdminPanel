c982be9c2d87cf3971aeb67044f57d2804f925ee
Chamika Jayarathna <failgames.lk@gmail.com>
2025-02-22 09:06:01 +0530

    Header update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,30 @@
 import React from 'react';
 
 const Header = () => {
+  const toggleSidebar = () => {
+    document.body.classList.toggle('sidebar-collapse');
+  };
+
   return (
     <div>
       <nav className="app-header navbar navbar-expand bg-body">
         <div className="container-fluid">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link" data-lte-toggle="sidebar" href="#" role="button">
+              <button
+                className="nav-link bg-transparent border-0"
+                onClick={toggleSidebar}
+                style={{ cursor: 'pointer' }}
+                role="button"
+              >
                 <i className="bi bi-list" />
-              </a>
+              </button>
             </li>
           </ul>
         </div>
       </nav>
     </div>
   );
-}
+};
 
 export default Header;
